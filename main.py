2a2372cbbf669b6da4f71e11a9db81f83ecc3dee
johnsirmon <johnsirmon@hotmail.com>
2023-12-16 09:43:28 -0500

    added additional functions to describe code

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import pkg_resources
 
 def count_user_python_files(root_dir):
     """Counts the number of Python files created by the user."""
@@ -64,15 +65,40 @@ def determine_virtual_env_manager():
 
     return virtual_env_manager
 
+def get_non_standard_libraries():
+    """Returns a list of non-standard libraries used in the project."""
+    standard_libs = sys.stdlib_module_names
+    installed_libs = {dist.key for dist in pkg_resources.working_set}
+    non_standard_libs = installed_libs - set(standard_libs)
+    return non_standard_libs
+
+def format_directory_structure(root_dir, max_depth=1):
+    """Formats the directory structure for the root."""
+    structure = []
+    for root, dirs, files in os.walk(root_dir):
+        depth = root.count(os.sep) - root_dir.count(os.sep)
+        if depth > max_depth:
+            continue
+        relative_path = os.path.relpath(root, root_dir)
+        if relative_path != '.':
+            structure.append(relative_path)
+    return ', '.join(structure)
+
+# Main execution
 virtual_env_manager = determine_virtual_env_manager()
 print(f"The developer is using {virtual_env_manager} to manage the virtual environment.")
 
 root_directory = '.'  # Root of your project
 python_file_count = count_user_python_files(root_directory)
 python_version = sys.version.split()[0]
+non_standard_libs = get_non_standard_libraries()
+dir_structure = format_directory_structure(root_directory)
+
 description = f"The developer has created {python_file_count} Python files using Python {python_version}."
-#check if the user is using a virtual environment
-if determine_virtual_env_manager() is not None:
+if virtual_env_manager is not None:
     description += f" They are using {virtual_env_manager} to manage their virtual environment."
-    
+if non_standard_libs:
+    description += f" Non-standard libraries used: {', '.join(non_standard_libs)}."
+description += f" Directory structure includes: {dir_structure}"
+
 print(description)
