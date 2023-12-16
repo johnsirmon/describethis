41406f710bc7ba725a61b6f71631e56ec7dec237
johnsirmon <johnsirmon@hotmail.com>
2023-12-16 09:05:19 -0500

    modified code to copilot suggested changes

--- main.py
+++ main.py
@@ -1,53 +1,25 @@
 import os
-import sys
-import pkg_resources
 
-def count_python_files(root_dir):
-    """Counts the number of Python files in the directory."""
-    python_file_count = sum([len([f for f in files if f.endswith('.py')]) for r, d, files in os.walk(root_dir)])
+def count_user_python_files(root_dir):
+    """Counts the number of Python files created by the user."""
+    python_file_count = 0
+    for r, d, files in os.walk(root_dir):
+        for file in files:
+            if file.endswith('.py') and not is_virtual_env_file(r, file):
+                python_file_count += 1
     return python_file_count
 
-def check_venv(root_dir):
-    """Checks for a virtual environment in the directory."""
-    for item in os.listdir(root_dir):
-        full_path = os.path.join(root_dir, item)
-        if os.path.isdir(full_path) and ('Scripts' in os.listdir(full_path) or 'bin' in os.listdir(full_path)):
-            return True, item
-    return False, None
-
-def get_non_standard_libraries():
-    """Returns a list of non-standard libraries used in the project."""
-    standard_libs = sys.stdlib_module_names
-    installed_libs = {dist.key for dist in pkg_resources.working_set}
-    non_standard_libs = installed_libs - set(standard_libs)
-    return non_standard_libs
-
-def format_directory_structure(root_dir):
-    """Formats the directory structure for the root."""
-    files = []
-    dirs = []
-    for item in os.listdir(root_dir):
-        if os.path.isfile(os.path.join(root_dir, item)):
-            files.append(item)
-        elif os.path.isdir(os.path.join(root_dir, item)):
-            dirs.append(item)
-    formatted_files = ', '.join(files)
-    formatted_dirs = ', '.join(dirs)
-    return formatted_files, formatted_dirs
-
-def format_output(python_file_count, venv, non_standard_libs, python_version, files, dirs):
-    """Formats the output for readability."""
-    venv_str = 'uses venv to create a virtual environment' if venv else 'does not use virtual environment'
-    lib_str = 'does not have any non-standard libraries that have been installed' if not non_standard_libs else f'uses non-standard libraries: {", ".join(non_standard_libs)}'
-    files_str = f'The root folder contains the files: {files}'
-    dirs_str = f'and the directories are: {dirs}' if dirs else ''
-    return f"I have a project I'm working on in VSCode, that uses Python {python_version} and has {python_file_count} Python file(s). {venv_str}. {lib_str}. {files_str} {dirs_str}"
+def is_virtual_env_file(directory, file):
+    """Checks if the file is a virtual environment-related file."""
+    virtual_env_files = ['.venv', 'venv', 'conda']
+    for venv_file in virtual_env_files:
+        if venv_file in directory or venv_file in file:
+            return True
+    return False
 
 root_directory = '.'  # Root of your project
-python_file_count = count_python_files(root_directory)
-venv_used, venv_name = check_venv(root_directory)
-non_standard_libs = get_non_standard_libraries()
+python_file_count = count_user_python_files(root_directory)
 python_version = sys.version.split()[0]
-files, dirs = format_directory_structure(root_directory)
-description = format_output(python_file_count, venv_used, non_standard_libs, python_version, files, dirs)
+venv_used = 'venv' if 'venv' in root_directory else 'conda' if 'conda' in root_directory else 'None'
+description = f"I have created {python_file_count} Python file(s) using Python {python_version}. I am using {venv_used} to manage the virtual environment."
 print(description)
